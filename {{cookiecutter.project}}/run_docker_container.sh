4728d314bf97350dbdb466f5898e5aaeb549e9f3
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-09-06 15:26:35 +0100

    Added sharing of bash_history file

--- {{cookiecutter.project}}/run_docker_container.sh
+++ {{cookiecutter.project}}/run_docker_container.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 CONTAINER={{ cookiecutter.project }}
-docker run -it --rm -v `pwd`/data:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
+touch `pwd`/bash_history
+docker run -it --rm -v `pwd`/bash_history:/root/.bash_history -v `pwd`/data:/data:ro -v `pwd`/scripts:/scripts:ro -v `pwd`/output:/output $CONTAINER
