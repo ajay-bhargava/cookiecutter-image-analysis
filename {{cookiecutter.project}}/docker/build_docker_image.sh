db01f5b09bc182d30e81f3b9f45e49e7843d49c6
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2016-09-08 13:44:31 +0100

    build_docker_image.sh now records the installed versions of python packages and writes them into requirements.txt at the top level

--- {{cookiecutter.project}}/docker/build_docker_image.sh
+++ {{cookiecutter.project}}/docker/build_docker_image.sh
@@ -5,5 +5,6 @@ IMAGE_NAME={{ cookiecutter.project }}
 cp ../requirements.txt $IMAGE_NAME
 cd $IMAGE_NAME
 docker build --no-cache -t $IMAGE_NAME .
-rm requirements.txt
+docker run --rm $IMAGE_NAME pip freeze > requirements.txt
+mv requirements.txt ../..
 cd ../
