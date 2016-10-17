9af2863c2f0669eb703aa30c36d73637bbe25ab1
Matthew Hartley <Matthew.Hartley@jic.ac.uk>
2016-10-17 16:01:02 +0100

    Added sleep after copying requirements.txt to avoid docker problems with pip

--- {{cookiecutter.project}}/docker/build_docker_image.sh
+++ {{cookiecutter.project}}/docker/build_docker_image.sh
@@ -3,6 +3,7 @@
 IMAGE_NAME="{{ cookiecutter.project }}"
 
 cp ../requirements.txt $IMAGE_NAME
+sleep 1
 cd $IMAGE_NAME
 docker build --no-cache -t $IMAGE_NAME .
 docker run --rm $IMAGE_NAME pip freeze > requirements.txt
